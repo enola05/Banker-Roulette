5ff7939cfe8e547ec2de686a86b8ffbe4d08e38f
Enola Lesni Coelho <69950177+enola05@users.noreply.github.com>
2022-05-17 13:02:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
 import random
-test_seed = int(input("Create a seed number: "))
-random.seed(test_seed)
 
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
